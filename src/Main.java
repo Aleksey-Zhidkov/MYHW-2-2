1987d42d877a7b81d9e8270b3fdd89e1a8f04ef8
Aleksey_Zhidkov <aleksey_zhidkov@mail.ru>
2026-02-09 11:34:54 +0300

    third

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
         int check = 100;
         int contribute = 1100;
         int bonus = (contribute / 100);
-        if (contribute >= 1001) {
+        if (contribute > 1000) {
             System.out.println(bonus + check + contribute);
+            System.out.println(bonus);
         } else {
             System.out.println(contribute + check);
+            System.out.println(bonus = 0);
         }
     }
 }
