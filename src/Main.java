bbdda802bec07ba97f2ecabea4f44c1b235b9fe2
Aleksey_Zhidkov <aleksey_zhidkov@mail.ru>
2026-02-09 13:25:58 +0300

    4

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,14 @@ public class Main {
     public static void main(String[] args) {
         int check = 100;
         int contribute = 1100;
-        int bonus = (contribute / 100);
         if (contribute > 1000) {
-            System.out.println(bonus + check + contribute);
-            System.out.println(bonus);
+            int bonus = (contribute / 100);
+            System.out.println("Баланс" + " " + (bonus + check + contribute));
+            System.out.println("Начисленных бонусов" + " " + bonus);
         } else {
-            System.out.println(contribute + check);
-            System.out.println(bonus = 0);
+            int bonus = 0;
+            System.out.println("Баланс" + " " + (contribute + check));
+            System.out.println("Начисленных бонусов" + " " + bonus);
         }
     }
 }
