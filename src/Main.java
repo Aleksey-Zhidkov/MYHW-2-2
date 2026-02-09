7602256f3d252137334d595a6917cac3e46b817e
Aleksey_Zhidkov <aleksey_zhidkov@mail.ru>
2026-02-09 11:23:31 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int check = 300;
-        int contribute = 1000;
+        int check = 100;
+        int contribute = 1100;
         int bonus = (contribute / 100);
-        if (contribute >= 1000) {
+        if (contribute >= 1001) {
             System.out.println(bonus + check + contribute);
         } else {
             System.out.println(contribute + check);
